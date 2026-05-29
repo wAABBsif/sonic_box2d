0256507021fd456931803eae4e4e2ab08475701e
wAABBsif <wasifkashifjaved@outlook.com>
2026-05-28 20:11:02 -0400

    Implement collision capsule

---
+++ src/phys/collision_shapes/collision_capsule.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "box2d/collision.h"
+#include "glm/vec2.hpp"
+#include "phys/collision_shape.hpp"
+
+namespace sb2d::phys
+{
+    class collision_capsule : public collision_shape
+    {
+    protected:
+        b2Capsule capsule;
+    public:
+        collision_capsule(const float density, const float friction, const float bounce, const float radius, const glm::vec2 center1, const glm::vec2 center2);
+    };
+}
